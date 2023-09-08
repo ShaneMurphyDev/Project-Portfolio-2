49556a9f78b743fd8ccc2b72f4b02717a30d89e1
ShaneMurphyDev <>
2023-09-08 12:26:51 +0000

    added scoretext to js

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,5 +1,5 @@
 const question = document.querySelector('#question');
 const choices = document.querySelector('.choice-text');
 const progressText = document.querySelector('#progressText');
-const question = document.querySelector('#question');
+const scoreText = document.querySelector('#score');
 const question = document.querySelector('#question');
