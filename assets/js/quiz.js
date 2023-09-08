1465eb22bd13df09c42926e2e18d12ca20403664
ShaneMurphyDev <>
2023-09-08 12:28:10 +0000

    added progressbar to js

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -2,4 +2,6 @@ const question = document.querySelector('#question');
 const choices = document.querySelector('.choice-text');
 const progressText = document.querySelector('#progressText');
 const scoreText = document.querySelector('#score');
-const question = document.querySelector('#question');
+const progressBarFull = document.querySelector('#progressBarFull');
+
+
