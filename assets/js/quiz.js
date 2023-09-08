c66940127059a81abcb3158c6c7b323e8eadef18
ShaneMurphyDev <>
2023-09-08 12:24:20 +0000

    created const questions in js

---
+++ assets/js/quiz.js
@@ -0,0 +1,5 @@
+const question = document.querySelector('#question');
+const question = document.querySelector('#question');
+const question = document.querySelector('#question');
+const question = document.querySelector('#question');
+const question = document.querySelector('#question');
