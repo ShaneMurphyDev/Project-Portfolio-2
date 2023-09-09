50d66b3a4f5c3a127b9d8ecdab4a4f8b2ef12f14
ShaneMurphyDev <>
2023-09-09 14:45:21 +0000

    fixed syntax error

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -4,17 +4,17 @@ const progressText = document.querySelector('#progressText');
 const scoreText = document.querySelector('#score');
 const progressBarFull = document.querySelector('#progressBarFull');
 
-let currentQuestion = {}
-let acceptingAnswers = true
-let score = '0'
-let questionCounter = '0'
-let availableQuestions = []
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = '0';
+let questionCounter = '0';
+let availableQuestions = [];
 
 
 let questions = [
     {
-        question:'What color is a Polar Bears skin?',
-        choice1:'White',
+        question: 'What color is a Polar Bears skin?',
+        choice1: 'White',
         choice2: 'Green',
         choice3: 'Yellow',
         choice4: 'Black',
@@ -92,66 +92,63 @@ let questions = [
         choice4: 'Ocelot',
         answer: 2,
     },
-]
+];
 
-const SCORE_POINTS: 1
-const MAX_QUESTIONS: 10
+const SCORE_POINTS: 1;
+const MAX_QUESTIONS: 10;
 
 startGame = () => {
-    questionCounter = 0
-    score = 0
-    availableQuestions = (...questions)
-    getNewQuestion()
-}
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    getNewQuestion();
+};
 
 getNewQuestion = () => {
-    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
-        localStorage.setItem('mostRecentScore', score)
+    if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score);
 
-        return window.location.assign('/end.html')
+        return window.location.assign('/end.html');
     }
 
-        questionCounter++
-        progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
-        progressBarFull.style.width =`${(questionCounter/MAX_QUESTIONS) * 100}%`
+    questionCounter++;
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`;
+    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
 
-        const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
-        currentQuestion = availableQuestions[questionsIndex]
-        question.innerText = currentQuestio.question
-
-        choices.forEach(choice => {
-            const number = choice.dataset ['number']
-            choice.innerText = currentQuestion['choice' + number]
-        })
-
-        availableQuestions.slice(questionsIndex, 1)
-        acceptingAnswers = true
-    }
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionsIndex];
+    question.innerText = currentQuestion.question;
 
     choices.forEach(choice => {
-        choice.addEvenListener('click', e, => {
-            if(!acceptingAnswers) return
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+    });
+
+    availableQuestions.slice(questionsIndex, 1);
+    acceptingAnswers = true;
+};
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptingAnswers) return
 
             acceptingAnswers = false
             const selectedChoice = e.target
-            const selectedAnswer = selectedChoice.dataset{'number'}
+            const selectedAnswer = selectedChoice.dataset['number']
 
             let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
 
             if(classToApply === 'correct') {
-                incrementScore(SCORE_POINTS)
-            }
+        incrementScore(SCORE_POINTS);
+    }
 
-            selectedChoice.parentElement.classList.add(classToApply)
+    selectedChoice.parentElement.classList.add(classToApply);
 
-            setTimeout{() =>{
-                selectedChoice.parentElement.classList.remove(classToApply)
-                getNewQuestion()
-            }, 10
-        }
-
-            
-        })
+    setTimeout(() => {
+        selectedChoice.parentElement.classList.remove(classToApply);
+        getNewQuestion();
+    }, 10);
+})
     })
 
 
