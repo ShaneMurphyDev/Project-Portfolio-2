3e3fcb8a9fa37b3fd9c3b6203ae6467855b5cdeb
ShaneMurphyDev <>
2023-09-09 21:17:54 +0000

    fixing syntax error

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,7 +1,7 @@
 const username = document.querySelector('#username')
 const saveScoreBtn = document.querySelector('#saveScoreBtn');
 const finalScore = document.querySelector('#finalScore');
-const mostRecentScore = document.querySelector('#mostRecentScore');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
 
 const highScores = JSON.parse(localStorage.getItem('highScores')) || []
 const MAX_HIGH_SCORES = 3
@@ -19,14 +19,14 @@ saveHighScore = e => {
 
     highScores.push(score)
 
-    highScores.sort((a,b,) => {
-        return b.score - a.score
-    })
+    highScores.sort((a, b) => {
+    return b.score - a.score;
+    });
+
 
     highScores.splice(3)
 
     localStorage.setItem('highScores', JSON.stringify(highScores))
-    window.location,assign('/')
-
+    window.location.assign('/');
 
 }
