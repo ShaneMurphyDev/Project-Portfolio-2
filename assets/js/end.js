48b935ee04e505981c853aec7bc872a86a95cfef
ShaneMurphyDev <>
2023-09-11 10:00:42 +0000

    updated file path

--- assets/js/end.js
+++ assets/js/end.js
@@ -27,6 +27,6 @@ saveHighScore = e => {
     highScores.splice(3)
 
     localStorage.setItem('highScores', JSON.stringify(highScores))
-    window.location.assign('/');
+    window.location.assign('leaderboard.html');
 
 }
